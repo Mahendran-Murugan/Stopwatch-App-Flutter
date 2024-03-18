eb38803fa7ec0049d788e8f8a5e7a5ebbe027f74
Mahendran-Murugan <mahendran1482004@gmail.com>
2024-03-18 21:25:23 +0530

    feat: complete stopwatch and lean timer

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,197 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int seconds = 0, minutes = 0, hours = 0;
+  String digitSeconds = '00', digitMinutes = '00', digitHours = '00';
+  Timer? timer;
+  bool isStarted = false;
+  List laps = [];
+
+  void stop() {
+    timer!.cancel();
+    setState(() {
+      isStarted = false;
+    });
+  }
+
+  void reset() {
+    timer!.cancel();
+    setState(() {
+      seconds = 0;
+      minutes = 0;
+      hours = 0;
+      digitSeconds = '00';
+      digitMinutes = '00';
+      digitHours = '00';
+      isStarted = false;
+    });
+  }
+
+  void addLaps() {
+    String lap = "$digitHours:$digitMinutes:$digitSeconds";
+    setState(() {
+      laps.add(lap);
+    });
+  }
+
+  void start() {
+    isStarted = true;
+    timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      int localSeconds = seconds + 1;
+      int localMinutes = minutes;
+      int localHours = hours;
+      if (localSeconds > 59) {
+        localMinutes += 1;
+        localSeconds = 0;
+      }
+      if (localMinutes > 59) {
+        localHours += 1;
+        localMinutes = 0;
+      }
+      setState(() {
+        seconds = localSeconds;
+        minutes = localMinutes;
+        hours = localHours;
+        digitSeconds = (seconds < 10) ? "0$seconds" : "$seconds";
+        digitMinutes = (minutes < 10) ? "0$minutes" : "$minutes";
+        digitHours = (hours < 10) ? "0$hours" : "$hours";
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(30.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Center(
+                child: Text(
+                  "S T O P W A T C H",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Center(
+                child: Text(
+                  "$digitHours:$digitMinutes:$digitSeconds",
+                  style: const TextStyle(
+                    color: Colors.black,
+                    fontSize: 50,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+              Container(
+                height: 400,
+                decoration: BoxDecoration(
+                  color: Colors.black12,
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                child: ListView.builder(
+                  itemCount: laps.length,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.all(15),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text(
+                            "Lap ${index + 1}",
+                            style: const TextStyle(
+                              color: Colors.black,
+                              fontSize: 18,
+                            ),
+                          ),
+                          Text(
+                            "${laps[index]}",
+                            style: const TextStyle(
+                              color: Colors.black,
+                              fontSize: 18,
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: RawMaterialButton(
+                      onPressed: () => {
+                        isStarted ? stop() : start(),
+                      },
+                      shape: const StadiumBorder(
+                        side: BorderSide(color: Colors.blueGrey),
+                      ),
+                      child: Text(
+                        isStarted ? "Stop" : "Start",
+                        style: const TextStyle(
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  IconButton(
+                    onPressed: () => {addLaps()},
+                    icon: const Icon(Icons.flag),
+                  ),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  Expanded(
+                    child: RawMaterialButton(
+                      onPressed: () => {
+                        reset(),
+                      },
+                      shape: const StadiumBorder(
+                        side: BorderSide(color: Colors.blueGrey),
+                      ),
+                      child: const Text(
+                        "Reset",
+                        style: TextStyle(
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
